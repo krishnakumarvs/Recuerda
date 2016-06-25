4e67ace79159e7bd49c819eafdad911746fed03d
suhailaabdulla <suhailaabdulla123@gmail.com>
2016-06-25 12:46:24 +0530

    Initial project structure is added.

---
+++ app/app.module.js
@@ -0,0 +1,78 @@
+var appName = "Recuerda";
+// Ionic Starter App
+
+
+angular.module(appName, ['ionic', 'LocalStorageModule'])
+
+.run(function($ionicPlatform, $rootScope, $state) {
+    $rootScope.$state = $state;
+
+    $ionicPlatform.ready(function() {
+        
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
+
+    $rootScope.$on('$stateChangeStart', function(e, curr, prev) {
+
+        //console.log(curr.title);
+        $rootScope.pageTitle = curr.title;
+    });
+    $rootScope.$on('$stateChangeSuccess', function(e, curr, prev) {
+        $rootScope.currentState = $state.current;
+
+    });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+    //fallback language if entry is not found in current language
+    /*$translateProvider.fallbackLanguage('es');*/
+    //load language entries from files
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+
+
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+
+    .state('header', {
+        url: "/",
+        templateUrl: "app/shared/templates/header.html",
+        /*controller: 'HeaderController as Header',*/
+        title: 'Header'
+    })
+
+    .state('login', {
+        url: "/login",
+        templateUrl: "app/login/templates/login.html",
+        controller: 'LoginController as Login',
+        title: 'Login'
+    });
+
+    //mockResult
+
+    /*.state('header.dashboard', {
+        url: "dashboard",
+        title: 'DashBoard',
+        parent: 'header',
+        showHeader: true,
+        views: {
+            'menuContent': {
+                templateUrl: "app/templates/dashboard.html",
+                controller: 'HomePageController as HomePage'
+            }
+        }
+    })*/
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/login');
+
+});
