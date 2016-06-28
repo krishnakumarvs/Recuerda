dbb13558bd264f750621d92bd3fb9126426b17a9
jeenapv <jeenapv1992@gmail.com>
2016-06-28 16:05:30 +0530

    controller for profile is added profile.html is created. intex.html is modified. settings.html is modified.

---
+++ app/profile/controller/controller.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+
+    /**
+     * Get the main module (shared for Workout).
+     */
+    angular.module(appName)
+    /**
+     * Login Controller.
+     */
+    .controller('ProfileController', Profile);
+
+    Profile.$inject = ['$state'];
+
+    function Profile($state) {
+        var loginVm = this;
+        
+        
+                  
+        
+    }
+
+})();
