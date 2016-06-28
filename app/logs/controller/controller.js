457d9e5988db9859912c964520b9262ad9efedb5
suhailaabdulla <suhailaabdulla123@gmail.com>
2016-06-28 13:10:15 +0530

    logs controller changed

--- app/logs/controller/controller.js
+++ app/logs/controller/controller.js
@@ -1,3 +1,5 @@
+
+
 (function() {
     'use strict';
 
@@ -8,12 +10,45 @@
     /**
      * Login Controller.
      */
-    .controller('LogsController', Log);
+    .controller('LogsController', Logs);
 
     Logs.$inject = ['$state'];
 
     function Logs($state) {
         var logsVm = this;
+        logsVm.allItems=reminder;
+        var reminder  = [
+        {
+            name : "gfh",
+            date : "123",
+            status: "done"
+        },
+        {
+            name : "bak",
+            date : "123",
+            status: "done"
+        },
+        {
+            name : "dLjddaj",
+            date : "123",
+            status: "done"
+        }
+        ];
+        var tasks= [
+        {
+            name : "gfh",
+            date : "123",
+            status: "done"
+        }
+        ];
+
+        logsVm.allItems=reminder;
+        function remind(){
+            logsVm.allItems=reminder;
+        }
+        function task(){
+            logsVm.allItems=task;
+        }
         
         /*loginVm.reg_num = "234";
         loginVm.password = "123"
@@ -42,5 +77,4 @@
             });
         }*/
     }
-
 })();
