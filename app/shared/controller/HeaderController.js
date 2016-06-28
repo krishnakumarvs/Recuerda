3e5c0a9a092543a1cd6a67bc9cb6e769958cf06c
suhailaabdulla <suhailaabdulla123@gmail.com>
2016-06-28 15:59:05 +0530

    state given from slide-menu.

--- app/shared/controller/HeaderController.js
+++ app/shared/controller/HeaderController.js
@@ -15,6 +15,36 @@
     function Header($state) {
         var headerVm = this;
         console.log("started header");
+        headerVm.goToPage=goToPage;
+    function goToPage(pageNo){
+       switch(pageNo) {
+           case 1:
+               $state.go('header.tasks')
+               break;
+           case 2:
+               $state.go('header.reminder')
+               break;
+
+            case 3:
+               $state.go('header.profile')
+               break;
+           case 4:
+               $state.go('header.settings')
+               break;
+           case 5:
+               $state.go('header.logs')
+               break;
+           case 6:
+               $state.go('login')
+               break;
+               
+           default: 
+                   $state.go('header.tasks')
+               
+       }
+
+    }
+    
     }
 
 })();
