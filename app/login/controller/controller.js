bdf1c1e331d4150520cdf72763a12bad2ff015d9
krishnakumarvs <krishh_mea@yahoo.in>
2016-07-16 18:28:04 +0530

    Firebase sdk details added in index file. Login controller calls firebase authentication for user SignUp.

--- app/login/controller/controller.js
+++ app/login/controller/controller.js
@@ -5,42 +5,41 @@
      * Get the main module (shared for Workout).
      */
     angular.module(appName)
-        /**
-         * Login Controller.
-         */
-        .controller('LoginController', Login);
+    /**
+     * Login Controller.
+     */
+    .controller('LoginController', Login);
 
     Login.$inject = ['$state', '$filter', 'LoginDataService'];
 
     function Login($state, $filter, LoginDataService) {
         var loginVm = this;
+        // Variable declarations
+        loginVm.currentUser = {};
+        loginVm.currentUser.email = "";
+        loginVm.currentUser.password = "";
+
+        // Function declarations
         loginVm.LoginFun = LoginFun;
         loginVm.SignUp = SignUp;
 
-        loginVm.currentUser = {};
-
-        loginVm.currentUser.email = "";
-        loginVm.currentUser.password = "";
-        var confirmPassword = "";
         activate();
 
         function activate() {
 
-            var temp = LoginDataService.currentUser(); 
-            
-            console.log(temp);
-
-            // loginVm.currentUser.email = temp.email;
-            // loginVm.currentUser.password = temp.password;
         }
 
 
         function LoginFun() {
-            $state.go('header.tasks');
+            firebase.auth().signInWithEmailAndPassword(loginVm.currentUser.email, loginVm.currentUser.password).then(function(response) {
+                //console.log(response);
+                $state.go('header.tasks');
+            }).catch(function(error) {
+                alert(error.message);
+            });
         }
 
         function SignUp() {
-
             $state.go('registration');
         }
     }
