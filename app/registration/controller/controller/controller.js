dd24d53439e8ec6b7be7ecdae892fa25985b70ae
krishnakumarvs <krishh_mea@yahoo.in>
2016-07-16 22:53:27 +0530

    Code intended and removed console log.

--- app/registration/controller/controller/controller.js
+++ app/registration/controller/controller/controller.js
@@ -5,36 +5,34 @@
      * Get the main module (shared for Workout).
      */
     angular.module(appName)
-        /**
-         * Registration Controller.
-         */
-        .controller('RegistrationController', Registration);
+    /**
+     * Registration Controller.
+     */
+    .controller('RegistrationController', Registration);
 
     Registration.$inject = ['$state', '$filter', 'RegistrationDataService'];
 
     function Registration($state, $filter, RegistrationDataService) {
         var registrationVm = this;
-        registrationVm.confirmPassword=confirmPassword;
-        registrationVm.addNewUser=addNewUser;
+        registrationVm.confirmPassword = confirmPassword;
+        registrationVm.addNewUser = addNewUser;
         registrationVm.newUser = {};
 
         registrationVm.newUser.email = "";
         registrationVm.newUser.password = "";
         registrationVm.newUser.userName = "";
-         var confirmPassword = "";
+        var confirmPassword = "";
 
         function addNewUser() {
-            console.log(111)
-            if(registrationVm.newUser.password ==registrationVm.confirmPassword)
-            {
-             RegistrationDataService.addNewUser(registrationVm.newUser);  
-             $state.go('login');
-            }
-            else{
+            if (registrationVm.newUser.password == registrationVm.confirmPassword) {
+                //RegistrationDataService.addNewUser(registrationVm.newUser);  
+                //$state.go('login');
+
+            } else {
                 console.log("password does not match");
             }
         }
-        
+
     }
 
 })();
