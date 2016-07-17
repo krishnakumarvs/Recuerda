ab596e390954d3e4b02dd8ba1d74b6a8353f6a37
jeenapv <jeenapv1992@gmail.com>
2016-07-17 13:31:45 +0530

    Profile service is aaded. Code added to view profile details in profile.html profile/controller/controller.js is modified.

--- app/profile/controller/controller.js
+++ app/profile/controller/controller.js
@@ -5,19 +5,43 @@
      * Get the main module (shared for Workout).
      */
     angular.module(appName)
-    /**
-     * Login Controller.
-     */
-    .controller('ProfileController', Profile);
+        /**
+         * Profile Controller.
+         */
+        .controller('ProfileController', Profile);
 
-    Profile.$inject = ['$state'];
+    Profile.$inject = ['$state', '$filter', 'ProfileDataService'];
 
-    function Profile($state) {
-        var loginVm = this;
-        
-        
-                  
-        
+    function Profile($state, $filter, ProfileDataService) {
+        var profileVm = this;
+
+        profileVm.profileDetails = {}
+        profileVm.profileDetails.userName = "";
+        profileVm.profileDetails.email = "";
+
+        activate();
+
+        function activate() {
+
+
+            ProfileDataService.getProfileDetails().then(function(response) {
+                profileVm.profileDetails.userName = response;
+                console.log(response)
+            }).catch(function(error) {
+                alert(error);
+            });
+
+
+
+            /*ProfileDataService.getProfileDetails().then(function(response) {
+                console.log(response);
+            }).catch(function(error) {
+                console.log(error);
+                alert(error);
+            });*/
+        }
+
+
     }
 
 })();
