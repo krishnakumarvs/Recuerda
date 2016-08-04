8da79c7f927e64ecbf627702ae28e6267adda873
jeenapv <jeenapv1992@gmail.com>
2016-08-04 18:21:55 +0530

    viewtask.html is modified to display task details. Function added to get data from local storage in controller.js

--- app/viewtask/controller/controller.js
+++ app/viewtask/controller/controller.js
@@ -10,11 +10,29 @@
      */
     .controller('ViewTaskController', ViewTask);
 
-    ViewTask.$inject = ['$state'];
+    ViewTask.$inject = ['$state','$filter','ViewTaskDataService'];
 
-    function ViewTask($state) {
-        var loginVm = this;
-        
+    function ViewTask($state,$filter,ViewTaskDataService) {
+        var viewTaskVm = this;
+        viewTaskVm.getTaskDetails = {};
+       //viewTaskVm.getTaskDetails.taskName = "";
+        //viewTaskVm.getTaskDetails.taskDescription = "";
+       // viewTaskVm.getTaskDetails.taskDate = "";
+        //viewTaskVm.getTaskDetails.taskPriority = "";
+        activate();
+
+        function activate() {
+            ViewTaskDataService.getTaskDetails().then(function(taskDetails) {
+                console.log(taskDetails);
+                viewTaskVm.getTaskDetails.taskName = taskDetails.taskName;
+                viewTaskVm.getTaskDetails.taskDescription = taskDetails.taskDescription;
+                viewTaskVm.getTaskDetails.taskDate = taskDetails.taskDate;
+                viewTaskVm.getTaskDetails.taskPriority = taskDetails.taskPriority;
+
+            }).catch(function(error) {
+                // No user details found which means user haven't registered
+            });
+        }
         
                   
         
