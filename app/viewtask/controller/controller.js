d812a83b4a0d992c4d16876823c2edb88e244bda
jeenapv <jeenapv1992@gmail.com>
2016-08-08 17:11:55 +0530

    View task is modified to display remaining time. Function added to get remaining days in controller.js.

--- app/viewtask/controller/controller.js
+++ app/viewtask/controller/controller.js
@@ -5,19 +5,20 @@
      * Get the main module (shared for Workout).
      */
     angular.module(appName)
-    /**
-     * Login Controller.
-     */
-    .controller('ViewTaskController', ViewTask);
+        /**
+         * Login Controller.
+         */
+        .controller('ViewTaskController', ViewTask);
 
-    ViewTask.$inject = ['$state','$filter','ViewTaskDataService'];
+    ViewTask.$inject = ['$state', '$filter', 'ViewTaskDataService'];
 
-    function ViewTask($state,$filter,ViewTaskDataService) {
+    function ViewTask($state, $filter, ViewTaskDataService) {
         var viewTaskVm = this;
         viewTaskVm.getTaskDetails = {};
-       //viewTaskVm.getTaskDetails.taskName = "";
+        viewTaskVm.getDay = getDay;
+        //viewTaskVm.getTaskDetails.taskName = "";
         //viewTaskVm.getTaskDetails.taskDescription = "";
-       // viewTaskVm.getTaskDetails.taskDate = "";
+        // viewTaskVm.getTaskDetails.taskDate = "";
         //viewTaskVm.getTaskDetails.taskPriority = "";
         activate();
 
@@ -33,9 +34,27 @@
                 // No user details found which means user haven't registered
             });
         }
-        
-                  
-        
+
+        function getDay() {
+            var a = new Date().getTime();
+           // console.log(a); // Now
+            // console.log(a.toString());
+            var b = new Date(viewTaskVm.getTaskDetails.taskDate);
+            var d = (b - a); 
+            //console.log(b);
+             //console.log(111);
+            if (d > 0) {
+                // difference in milliseconds 
+                var oneDay = 24 * 60 * 60 * 1000;
+                var w = parseInt((d / oneDay)+1);
+               // console.log(d / oneDay);
+            } else if (d < 0) {
+               var w=0;
+            }
+            return w;
+        }
+
+
     }
 
 })();
